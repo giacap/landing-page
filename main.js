2bcd6aef52fe68b7768829b1b13cf35bf962eee9
giacap <79063411+giacap@users.noreply.github.com>
2021-03-08 00:49:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('go-to-services-cta').addEventListener('click', function(){
+    document.getElementById('section2').scrollIntoView({behavior: "smooth"});
+})
+
+
+document.getElementById('btn-submit').addEventListener('click', function(e){
+    e.preventDefault();
+});
